df81a8b86c0150cb3750964683c5707c33a86ba5
parthipan <parthipan@sacrasys.com>
2025-06-12 17:35:28 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, unused_import, use_build_context_synchronously
+// ignore_for_file: library_private_types_in_public_api, unused_import, use_build_context_synchronously//
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -788,6 +788,7 @@ class _SignupState extends State<Signup> {
                                 );
                                 if (status == 200) {
                                   Navigator.pushReplacement(
+                                    // ignore: use_build_context_synchronously
                                     context,
                                     MaterialPageRoute(
                                       builder: (context) => const Login(),
